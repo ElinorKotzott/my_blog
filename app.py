eec6439694bf9a9c14f813d3e5e9c4449d0502ec
Eli_Nor <elinor.kotzott@gmx.de>
2025-03-20 17:42:51 +0100

    added html and css files and copied index route

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -8,5 +9,14 @@ def hello_world():
     return 'Hello, World!'
 
 
+
+@app.route('/')
+def index():
+    # add code here to fetch the job posts from a file
+    return render_template('index.html', posts=blog_posts)
+
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
+
+
