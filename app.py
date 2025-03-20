4099af519036d0517a72e880f24a835f6c637298
Eli_Nor <elinor.kotzott@gmx.de>
2025-03-20 19:59:57 +0100

    added option to add a new post

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import json
-
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -11,6 +9,31 @@ def index():
     with open("data.json", "r") as handle:
         blog_posts = json.load(handle)
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        content = request.form['content']
+
+        with open("data.json", "r") as handle:
+            blog_posts = json.load(handle)
+
+        new_post = {
+            "id": len(blog_posts) + 1,
+            "title": title,
+            "author": author,
+            "content": content
+        }
+
+        blog_posts.append(new_post)
+
+        with open("data.json", "w") as handle:
+            json.dump(blog_posts, handle)
+
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
